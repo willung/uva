51da8db8cf0c9ed266e82be0a2e467088bf2bfcc
willung <willung@users.noreply.github.com>
2017-03-05 04:36:56 +1100

    Create problem 10082.cpp

---
+++ problem 10082.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+#define NO_KEYS 34
+
+using namespace std;
+
+string getReplaceStr(char input);
+
+int main()
+{
+   int i, index;
+   string input;
+   
+   cin >> input;
+   for (i=0; i<input.length; i++) {
+       input.replace(i,1,getReplaceStr(input[i]));
+   }
+   
+   cout << input << endl;
+   return 0;
+}
+
+string getReplaceStr(char input) {
+    int i;
+    char KEYS[NO_KEYS] = {'Q','W','E','R','T','Y','U','I','O','P','[',']','\\',
+                          'A','S','D','F','G','H','J','K','L',';','\'',
+                          'Z','X','C','V','B','N','M',',','.','/'};
+    for (i=1; i<NO_KEYS; i++) {
+        if (tolower(keys[i]) == tolower(input)) {
+            if (islower(input)) {
+                return string(1,tolower(keys[i]));
+            } else {
+                return string(1,keys[i]);
+            }
+        }
+        
+    }
+
