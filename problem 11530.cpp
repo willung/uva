c24d31b64f34d36eef462c70b32b93421b699825
willung <willung@users.noreply.github.com>
2017-03-05 20:28:42 +1100

    Update problem 11530.cpp

--- problem 11530.cpp
+++ problem 11530.cpp
@@ -9,10 +9,11 @@ int getPresses(char input);
 
 int main()
 {
-   int i,j,sum=0;
+   int i,j,sum=0, n_tcases;
    string input;
    
    cin >> n_tcases;
+   cin.ignore();
    for (i=0; i<n_tcases; i++) { 
        getline(cin,input);
        
@@ -27,8 +28,22 @@ int main()
 }
 
 int getPresses(char input) {
+    int initial_press=0;
     if (input == ' ') { 
         return 1;
     }
-    return (int(input)-int('a')) % 3;
+    
+    if (input < 's' ) {
+        // %3 divides into groups of 3 letters, results in 0, 1 or 2
+        return (int(input)-int('a')) % 3 + 1;
+    } else {
+        if (input == 's') {
+            return (int(input)-int('s')) % 4 + 4;
+        } else if (input < 'w') {
+            return (int(input)-int('s')) % 4;
+        } else {
+            return (int(input)-int('s')) % 4 + 1;
+        }
+        
+    }
 }
