fd6a65a8e64f8ce1194210b8fee2d78bd9998cf8
willung <willung@users.noreply.github.com>
2017-11-29 03:01:52 +1100

    Update 573 - The Snail.cpp

--- 573 - The Snail.cpp
+++ 573 - The Snail.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
    int wall_dist=1, climb_dist, fall_dist, fatigue;
-   double curr_dist=0, curr_day;
+   double newclimb_dist, curr_dist=0, curr_day;
    
    while(1) {
         cin >> wall_dist >> climb_dist >> fall_dist >> fatigue;
@@ -14,18 +14,22 @@ int main()
         if (wall_dist == 0) break;
         
         while(1) {
-            curr_dist += climb_dist*(1-(curr_day-1)*fatigue*1.0/100);
+            newclimb_dist = climb_dist*(1-(curr_day-1)*fatigue/100);
+            curr_dist += newclimb_dist*(newclimb_dist > 0);
             // cout << "day " << curr_day << ", dist: " << curr_dist << "\n";
-            if (curr_dist <= 0) {
-                cout << "failure on day " << curr_day << "\n";
-                break;
-            } else if (curr_dist > wall_dist) {
+            if (curr_dist > wall_dist) {
                 cout << "success on day " << curr_day << "\n";
                 break;
-            } else {
-                curr_dist -= fall_dist;
-                curr_day += 1;
             }
+            
+            curr_dist -= fall_dist;
+
+            if (curr_dist < 0) {
+                cout << "failure on day " << curr_day << "\n";
+                break;
+            } 
+            
+            curr_day += 1;
         }
    }
    return 0;
