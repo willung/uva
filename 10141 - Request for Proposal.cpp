f08f646441044eeaf0cce12ccae1e99d6c050b80
willung <willung@users.noreply.github.com>
2017-11-29 03:51:19 +1100

    Update 10141 - Request for Proposal.cpp

--- 10141 - Request for Proposal.cpp
+++ 10141 - Request for Proposal.cpp
@@ -8,24 +8,41 @@ using namespace std;
 
 int main()
 {
-    int i,j, n_reqs, n_props;
-    float price[MAX_PROPS], reqs_met[MAX_PROPS];
+    int i,j, n_reqs, n_props, reqs_met[MAX_PROPS], max_reqs, final_prop;
+    float price[MAX_PROPS], min_price;
     string temp, name[MAX_PROPS];
     cin >> n_reqs >> n_props;
-    for (i=0; i<=n_reqs; i++) {
+    cin.ignore(1,'\n');
+    for (i=0; i<n_reqs; i++) {
         getline(cin,temp);
         // cout << temp << "\n";
     }
     
-    for (i=0; i<=n_props; i++) {
+    for (i=0; i<n_props; i++) {
         cin >> name[i];
         cin >> price[i] >> reqs_met[i];
-        cout << name[i] << " " << price[i] << " "<< reqs_met[i] << "\n";
+        // cout << name[i] << " " << price[i] << " "<< reqs_met[i] << "\n";
         for (j=0; j<=reqs_met[i]; j++) {
             getline(cin,temp);
             // cout << temp << "\n";
         }
     }
     
+    final_prop = 0;
+    min_price = price[0];
+    max_reqs = reqs_met[0];
+    for (i=0; i<n_props; i++) {
+        if (reqs_met[i] > max_reqs) {
+            max_reqs = reqs_met[i];
+            min_price = price[i];
+            final_prop = i;
+        }
+        
+        if (price[i] < min_price && reqs_met[i] == max_reqs) {
+            min_price = price[i];
+            final_prop = i;
+        }
+    }
+    cout << name[final_prop] << '\n' ;
     return 0;
 }
